08d39d196a94b62f908171649d32de3e9152bbf3
yujin <yujin@yujins-MacBook-Air.local>
2020-09-30 21:57:27 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Reddit handle scraper</p>
+        <p>Reddit Handle Scraper</p>
         <a
           className="App-link"
           href="https://reactjs.org"
