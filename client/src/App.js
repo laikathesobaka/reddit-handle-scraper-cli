c68e51c8cb56defa5abdec69b88064c584b32d9c
yujin <yujin@yujins-MacBook-Air.local>
2020-10-04 20:27:17 -0700

    adds express server

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Search from "./Search";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Reddit Handle Scraper</p>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
