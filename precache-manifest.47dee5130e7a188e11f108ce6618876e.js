ddbf17c0384ec317c5a3e3a0f52d1fc3a85d0a4b
yujin <yujin@yujins-MacBook-Air.local>
2020-10-07 22:43:01 -0700

    Updates

---
+++ precache-manifest.47dee5130e7a188e11f108ce6618876e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4441f1a99a544bf8ae6f486b277707",
+    "url": "/reddit-handle-scraper/index.html"
+  },
+  {
+    "revision": "88533adf8bed8efd679c",
+    "url": "/reddit-handle-scraper/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "708590fa423613334330",
+    "url": "/reddit-handle-scraper/static/js/2.908d1194.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reddit-handle-scraper/static/js/2.908d1194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88533adf8bed8efd679c",
+    "url": "/reddit-handle-scraper/static/js/main.be1a9333.chunk.js"
+  },
+  {
+    "revision": "3844f5463306c1ab1835",
+    "url": "/reddit-handle-scraper/static/js/runtime-main.0f020936.js"
+  }
+]);
